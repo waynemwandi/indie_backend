3c8f57a28d88d24b25c6d931e918e08df9d75245
waynemwandi <mwandiwayne@gmail.com>
2024-07-26 19:57:21 +0300

    Set up .env file for security/sensitive variables

--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,10 @@
+
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ogx*x+@ql+9aawm3g)zaiti)!n@d&m^3g26-*9rnjqnkj7eies'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
